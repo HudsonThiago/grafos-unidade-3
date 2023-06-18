00f48d68c78c3b80567211d2bf4625fa58732272
Pedro Henrique T S <81829438+fluriafloral@users.noreply.github.com>
2023-06-18 15:33:16 -0300

    Update cobertura_vertices.py
    tá rodando, mas não imprime o resultado
    o problema provavelmente tá na função que checa se é uma cobertura

--- cobertura_vertices.py
+++ cobertura_vertices.py
@@ -1,22 +1,46 @@
-caminhoGrafo2 = "./casos de uso/grafo2.txt"
+class grafo:
+    def lerArquivo(self, file):
+        with open(file, 'r') as file:
+            list = [[int(num) for num in line.split(' ')] for line in file]
 
+            self.n = len(list)
+            self.m = 0
+            self.vertices = range(self.n)
+            self.listaAdj = {vertex : set() for vertex in self.vertices}
 
-def lerGrafo(caminhoGrafo):
-    arquivo = open(caminhoGrafo, "r")
+            for i in self.vertices:
+                for j in self.vertices:
+                    if list[i][j] == 1:
+                        self.listaAdj[i].add(j)
+                        self.listaAdj[j].add(i)
+                        if i < j:
+                            self.m += 1
+    
+    def cobertura(self, subconjunto):
+        vertices = self.listaAdj
+        for vertice in subconjunto:
+            if vertice in vertices:
+                for v in vertices[vertice]:
+                    if v in vertice:
+                        del vertice[v]
+                del vertices[vertice]
+        if len(vertices) == 0:
+            return True
+        else:
+            return False
 
-    arquivo.seek(0, 0)
+import itertools
 
-    grafo = []
+class coberturaMinimaVertice:
+    def guloso(self, graph):
+        for i in range(graph.n):
+            for subset in itertools.combinations(graph.vertices, i):
+                if graph.cobertura(subset):
+                    result = "subset: {subset}"
+                    return result
 
-    for i in arquivo.readlines():
-        verticesAdjacentes = list(map(int, i.split()))
-        grafo.append(verticesAdjacentes)
+g1 = grafo()
+g1.lerArquivo("./casos-de-uso/grafo1.txt")
 
-    arquivo.close()
-
-    return grafo
-
-
-grafo2 = lerGrafo(caminhoGrafo2)
-
-print(grafo2)
+alg = coberturaMinimaVertice()
+alg.guloso(g1)
